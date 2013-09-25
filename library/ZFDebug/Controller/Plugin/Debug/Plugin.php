7f0aa1aaaabeb3f11e8a13f4956c8397cfefb817
Krzysztof Bukowski <ja@krzysztofbukowski.pl>
2013-09-26 00:51:21 +0100

    Added colours to the Variables tab

--- library/ZFDebug/Controller/Plugin/Debug/Plugin.php
+++ library/ZFDebug/Controller/Plugin/Debug/Plugin.php
@@ -1,38 +1,28 @@
 <?php
+
 /**
  * ZFDebug Zend Additions
  *
  * @category   ZFDebug
- * @package    ZFDebug_Controller
+ * @package    Tid_ZFDebug_Controller
  * @subpackage Plugins
- * @copyright  Copyright (c) 2008-2009 ZF Debug Bar Team (http://code.google.com/p/zfdebug)
- * @license    http://code.google.com/p/zfdebug/wiki/License     New BSD License
  * @version    $Id: $
  */
+class ZFDebug_Controller_Plugin_Debug_Plugin {
 
-/**
- * @category   ZFDebug
- * @package    ZFDebug_Controller
- * @subpackage Plugins
- * @copyright  Copyright (c) 2008-2009 ZF Debug Bar Team (http://code.google.com/p/zfdebug)
- * @license    http://code.google.com/p/zfdebug/wiki/License     New BSD License
- */
-class ZFDebug_Controller_Plugin_Debug_Plugin
-{
+    const COLOR_RED = '#cc0000';
+    const COLOR_PINK = '#F55874';
+    const COLOR_GREEN = '#4e9a06';
+    const COLOR_BLUE = '#4C97B4';
+    const COLOR_VIOLET = '#75507b';
+
     protected $_closingBracket = null;
 
-    public function getLinebreak()
-    {
-        return '<br'.$this->getClosingBracket();
+    public function getLinebreak() {
+        return '<br' . $this->getClosingBracket();
     }
 
-    public function getIconData()
-    {
-        return 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAHhSURBVDjLpZI9SJVxFMZ/r2YFflw/kcQsiJt5b1ije0tDtbQ3GtFQYwVNFbQ1ujRFa1MUJKQ4VhYqd7K4gopK3UIly+57nnMaXjHjqotnOfDnnOd/nt85SURwkDi02+ODqbsldxUlD0mvHw09ubSXQF1t8512nGJ/Uz/5lnxi0tB+E9QI3D//+EfVqhtppGxUNzCzmf0Ekojg4fS9cBeSoyzHQNuZxNyYXp5ZM5Mk1ZkZT688b6thIBenG/N4OB5B4InciYBCVyGnEBHO+/LH3SFKQuF4OEs/51ndXMXC8Ajqknrcg1O5PGa2h4CJUqVES0OO7sYevv2qoFBmJ/4gF4boaOrg6rPLYWaYiVfDo0my8w5uj12PQleB0vcp5I6HsHAUoqUhR29zH+5B4IxNTvDmxljy3x2YCYUwZVlbzXJh9UKeQY6t2m0Lt94Oh5loPdqK3EkjzZi4MM/Y9Db3MTv/mYWVxaqkw9IOATNR7B5ABHPrZQrtg9sb8XDKa1+QOwsri4zeHD9SAzE1wxBTXz9xtvMc5ZU5lirLSKIz18nJnhOZjb22YKkhd4odg5icpcoyL669TAAujlyIvmPHSWXY1ti1AmZ8mJ3ElP1ips1/YM3H300g+W+51nc95YPEX8fEbdA2ReVYAAAAAElFTkSuQmCC';
-    }
-
-    public function getClosingBracket()
-    {
+    public function getClosingBracket() {
         if (!$this->_closingBracket) {
             if ($this->_isXhtml()) {
                 $this->_closingBracket = ' />';
@@ -44,49 +34,41 @@ class ZFDebug_Controller_Plugin_Debug_Plugin
         return $this->_closingBracket;
     }
 
-    protected function _isXhtml()
-    {
+    protected function _isXhtml() {
         $view = Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer')->view;
         $doctype = $view->doctype();
         return $doctype->isXhtml();
     }
 
-    /**
-     * Transforms data into readable format
-     *
-     * @param array $values
-     * @return string
-     */
-    protected function _cleanData($values)
-    {
+    protected function _cleanData($values) {
+
         $linebreak = $this->getLinebreak();
 
         if (is_array($values)) {
             ksort($values);
         }
         $retVal = '<div class="pre">';
-        foreach ($values as $key => $value)
-        {
+        foreach ($values as $key => $value) {
             $key = htmlspecialchars($key);
-            if (is_numeric($value)) {
-                $retVal .= $key.' => '.$value.$linebreak;
-            }
-            else if (is_string($value)) {
-                $retVal .= $key.' => \''.htmlspecialchars($value).'\''.$linebreak;
-            }
-            else if (is_array($value))
-            {
-                $retVal .= $key.' => '.self::_cleanData($value);
-            }
-            else if (is_object($value))
-            {
-                $retVal .= $key.' => '.get_class($value).' Object()'.$linebreak;
-            }
-            else if (is_null($value))
-            {
-                $retVal .= $key.' => NULL'.$linebreak;
+            if (is_string($value)) {
+                $retVal .= $key . ' => ' . $this->_format('string', htmlspecialchars($value), self::COLOR_RED) . $linebreak;
+            } else if (is_numeric($value)) {
+                $retVal .= $key . ' => ' . $this->_format('number', $value, self::COLOR_GREEN) . $linebreak;
+            } else if (is_array($value)) {
+                $retVal .= $key . ' => ' . self::_cleanData($value);
+            } else if (is_object($value)) {
+                $retVal .= $key . ' => ' . $this->_format('object', get_class($value), self::COLOR_PINK) . $linebreak;
+            } else if (is_null($value)) {
+                $retVal .= $key . ' => ' . $this->_format(null, 'null', self::COLOR_BLUE) . $linebreak;
+            } else if (is_bool($value)) {
+                $retVal .= $key . ' => ' . $this->_format('boolean', $value ? 'true' : 'false', self::COLOR_VIOLET) . $linebreak;
             }
         }
-        return $retVal.'</div>';
+        return $retVal . '</div>';
     }
+
+    protected function _format($type, $value, $color) {
+        return '<small>' . $type . '</small> <span style="color:' . $color . '">' . $value . '</span>';
+    }
+
 }
--- library/ZFDebug/Controller/Plugin/Debug/Plugin/Variables.php
+++ library/ZFDebug/Controller/Plugin/Debug/Plugin/Variables.php
@@ -1,24 +1,14 @@
 <?php
+
 /**
  * ZFDebug Zend Additions
  *
  * @category   ZFDebug
- * @package    ZFDebug_Controller
+ * @package    Tid_ZFDebug_Controller
  * @subpackage Plugins
- * @copyright  Copyright (c) 2008-2009 ZF Debug Bar Team (http://code.google.com/p/zfdebug)
- * @license    http://code.google.com/p/zfdebug/wiki/License     New BSD License
- * @version    $Id$
  */
+class ZFDebug_Controller_Plugin_Debug_Plugin_Variables extends ZFDebug_Controller_Plugin_Debug_Plugin implements ZFDebug_Controller_Plugin_Debug_Plugin_Interface {
 
-/**
- * @category   ZFDebug
- * @package    ZFDebug_Controller
- * @subpackage Plugins
- * @copyright  Copyright (c) 2008-2009 ZF Debug Bar Team (http://code.google.com/p/zfdebug)
- * @license    http://code.google.com/p/zfdebug/wiki/License     New BSD License
- */
-class ZFDebug_Controller_Plugin_Debug_Plugin_Variables extends ZFDebug_Controller_Plugin_Debug_Plugin implements ZFDebug_Controller_Plugin_Debug_Plugin_Interface
-{
     /**
      * Contains plugin identifier name
      *
@@ -32,12 +22,11 @@ class ZFDebug_Controller_Plugin_Debug_Plugin_Variables extends ZFDebug_Controlle
     protected $_request;
 
     /**
-     * Create ZFDebug_Controller_Plugin_Debug_Plugin_Variables
+     * Create Tid_ZFDebug_Controller_Plugin_Debug_Plugin_Variables
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
 
     }
 
@@ -46,8 +35,7 @@ class ZFDebug_Controller_Plugin_Debug_Plugin_Variables extends ZFDebug_Controlle
      *
      * @return string
      */
-    public function getIdentifier()
-    {
+    public function getIdentifier() {
         return $this->_identifier;
     }
 
@@ -55,9 +43,8 @@ class ZFDebug_Controller_Plugin_Debug_Plugin_Variables extends ZFDebug_Controlle
      * Returns the base64 encoded icon
      *
      * @return string
-     **/
-    public function getIconData()
-    {
+     * */
+    public function getIconData() {
         return 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAFWSURBVBgZBcE/SFQBAAfg792dppJeEhjZn80MChpqdQ2iscmlscGi1nBPaGkviKKhONSpvSGHcCrBiDDjEhOC0I68sjvf+/V9RQCsLHRu7k0yvtN8MTMPICJieaLVS5IkafVeTkZEFLGy0JndO6vWNGVafPJVh2p8q/lqZl60DpIkaWcpa1nLYtpJkqR1EPVLz+pX4rj47FDbD2NKJ1U+6jTeTRdL/YuNrkLdhhuAZVP6ukqbh7V0TzmtadSEDZXKhhMG7ekZl24jGDLgtwEd6+jbdWAAEY0gKsPO+KPy01+jGgqlUjTK4ZroK/UVKoeOgJ5CpRyq5e2qjhF1laAS8c+Ymk1ZrVXXt2+9+fJBYUwDpZ4RR7Wtf9u9m2tF8Hwi9zJ3/tg5pW2FHVv7eZJHd75TBPD0QuYze7n4Zdv+ch7cfg8UAcDjq7mfwTycew1AEQAAAMB/0x+5JQ3zQMYAAAAASUVORK5CYII=';
     }
 
@@ -66,18 +53,11 @@ class ZFDebug_Controller_Plugin_Debug_Plugin_Variables extends ZFDebug_Controlle
      *
      * @return string
      */
-    public function getTab()
-    {
+    public function getTab() {
         return ' Variables';
     }
 
-    /**
-     * Gets content panel for the Debugbar
-     *
-     * @return string
-     */
-    public function getPanel()
-    {
+    public function getPanel() {
         $this->_request = Zend_Controller_Front::getInstance()->getRequest();
         $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer');
         if ($viewRenderer->view && method_exists($viewRenderer->view, 'getVars')) {
@@ -85,16 +65,16 @@ class ZFDebug_Controller_Plugin_Debug_Plugin_Variables extends ZFDebug_Controlle
         } else {
             $viewVars = "No 'getVars()' method in view class";
         }
+
         $vars = '<div style="width:50%;float:left;">';
         $vars .= '<h4>View variables</h4>'
-              . '<div id="ZFDebug_vars" style="margin-left:-22px">' . $viewVars . '</div>'
-              . '<h4>Request parameters</h4>'
-              . '<div id="ZFDebug_requests" style="margin-left:-22px">' . $this->_cleanData($this->_request->getParams()) . '</div>';
+                . '<div id="ZFDebug_vars" style="margin-left:-22px">' . $viewVars . '</div>'
+                . '<h4>Request parameters</h4>'
+                . '<div id="ZFDebug_requests" style="margin-left:-22px">' . $this->_cleanData($this->_request->getParams()) . '</div>';
         $vars .= '</div><div style="width:45%;float:left;">';
-        if ($this->_request->isPost())
-        {
+        if ($this->_request->isPost()) {
             $vars .= '<h4>Post variables</h4>'
-                   . '<div id="ZFDebug_post" style="margin-left:-22px">' . $this->_cleanData($this->_request->getPost()) . '</div>';
+                    . '<div id="ZFDebug_post" style="margin-left:-22px">' . $this->_cleanData($this->_request->getPost()) . '</div>';
         }
 
         $registry = Zend_Registry::getInstance();
@@ -104,7 +84,7 @@ class ZFDebug_Controller_Plugin_Debug_Plugin_Variables extends ZFDebug_Controlle
 
         $cookies = $this->_request->getCookie();
         $vars .= '<h4>Cookies</h4>'
-               . '<div id="ZFDebug_cookie" style="margin-left:-22px">' . $this->_cleanData($cookies) . '</div>';
+                . '<div id="ZFDebug_cookie" style="margin-left:-22px">' . $this->_cleanData($cookies) . '</div>';
 
         $vars .= '</div><div style="clear:both">&nbsp;</div>';
         return $vars;
